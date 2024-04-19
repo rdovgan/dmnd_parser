c0e9ea5c78a3ddbfa3db56ae1d91ed74cb228e4e
Roman Dovgan <romadovg@gmail.com>
2024-04-20 00:15:41 +0300

    Implemented method to send an email with attachment

--- service/parser.py
+++ service/parser.py
@@ -1,4 +1,37 @@
 import requests
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import os
+from dotenv import load_dotenv
+
+
+output_file_name = 'output.txt'
+
+
+def send_email(receiver_email, subject, body, attachment_path=None):
+    sender_email = os.getenv("EMAIL_ADDRESS")
+    sender_password = os.getenv("EMAIL_PASSWORD")
+    message = MIMEMultipart()
+
+    message.attach(MIMEText(body, "plain"))
+
+    if attachment_path:
+        with open(attachment_path, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header("Content-Disposition", f"attachment; filename= {attachment_path.split('/')[-1]}")
+        message.attach(part)
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.send_message(message)
+
+    print("Email sent successfully")
 
 
 def search_github_repositories(query):
@@ -42,7 +75,7 @@ def validate_lines(file_url):
         return False
 
 
-def main():
+def parse():
     query = input("Enter search query for GitHub repositories: ")
     repositories = search_github_repositories(query)
 
@@ -53,9 +86,11 @@ def main():
         for file in files:
             file_url = f"https://raw.githubusercontent.com/{owner}/{repo_name}/master/{file}"
             if validate_lines(file_url):
-                with open('output.txt', 'w') as output_file:
+                with open(output_file_name, 'w') as output_file:
                     output_file.write(f"{file_url} in {owner}/{repo_name} repository is valid.")
 
 
 if __name__ == "__main__":
-    main()
+    receiver_address = os.getenv('RECEIVER_ADDRESS')
+    # parse()
+    send_email(receiver_address, 'Diamond inside', 'Result email with attachment', output_file_name)
