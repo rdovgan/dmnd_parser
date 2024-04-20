59becef17021f3a6e0cf37b6a703751ad4e83c3c
Roman Dovgan <romadovg@gmail.com>
2024-04-20 10:53:54 +0300

    Implemented users retrieve and store into file with continuous iteration

--- service/user_retrieve.py
+++ service/user_retrieve.py
@@ -1,8 +1,11 @@
 import requests
 import os
+from dotenv import load_dotenv
 
 
 users_file = 'users.txt'
+last_user_file = 'last_user.txt'
+load_dotenv()
 
 
 def read_users_from_file(file_path):
@@ -17,44 +20,58 @@ def read_users_from_file(file_path):
     return users
 
 
-def search_users_with_language(language, token):
-    url = "https://api.github.com/search/repositories"
-    params = {"q": f"language:{language}"}
-    headers = {"Authorization": f"token {token}"}
-    response = requests.get(url, params=params, headers=headers)
-    if response.status_code == 200:
-        repositories = response.json()["items"]
-        users = set()
-        for repo in repositories:
-            owner = repo["owner"]["login"]
-            users.add(owner)
-        return users
+def get_github_users(limit=100, since=None, per_page=100, token=None):
+    url = "https://api.github.com/users"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    if token:
+        headers["Authorization"] = f"Bearer {token}"
+    params = {
+        "since": since,
+        "per_page": per_page
+    }
+    all_users = []
+    for i in range(limit):
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            users = response.json()
+            if not users:
+                break
+            store_users_to_file(users, users_file)
+            all_users.extend(users)
+            since = users[-1]["id"]
+            params["since"] = since
+            with open(last_user_file, "w") as file:
+                file.write(since + "\n")
+        else:
+            print(f"Failed to retrieve users: {response.status_code}")
+            break
+    return all_users
+
+
+def get_since_from_file():
+    if os.path.isfile(last_user_file):
+        with open(last_user_file, "r") as file:
+            since = file.readline().strip()
+            return since
     else:
-        print(f"Failed to search for {language} repositories: {response.status_code}")
-        return set()
-
-
-def search_users_with_languages(languages, token):
-    users = set()
-    for language in languages:
-        users.update(search_users_with_language(language, token))
-    return users
+        return None
 
 
 def store_users_to_file(users, file_path):
+    existing_users = read_users_from_file(file_path)
+    new_users = {user_dict["login"] for user_dict in users}
+    combined_users = existing_users.union(new_users)
     with open(file_path, "w") as file:
-        for user in users:
+        for user in combined_users:
             file.write(user + "\n")
 
 
 def main():
-    languages = ["Go", "Rust", "JavaScript", "Python"]
     token = os.getenv('GITHUB_TOKEN')
-    existing_users = read_users_from_file(users_file)
-    new_users = search_users_with_languages(languages, token)
-    combined_users = existing_users.union(new_users)
-    store_users_to_file(combined_users, users_file)
-    print(f"Stored {len(combined_users)} unique users to {users_file}.")
+    get_github_users(token=token, since=get_since_from_file())
 
 
 if __name__ == "__main__":
